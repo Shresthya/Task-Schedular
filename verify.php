61cd3cbf09a253164a1a05d7c22a8c1e8c610d46
Shresthya <163310331+Shresthya@users.noreply.github.com>
2025-05-28 21:21:49 +0530

    verify.php

--- verify.php
+++ verify.php
@@ -1,18 +1,9 @@
 <?php
 require_once 'functions.php';
 
-// TODO: Implement verification logic.
-
-?>
-
-<!DOCTYPE html>
-<html>
-<head>
-	<!-- Implement Header ! -->
-</head>
-<body>
-	<!-- Do not modify the ID of the heading -->
-	<h2 id="verification-heading">Subscription Verification</h2>
-	<!-- Implemention body -->
-</body>
-</html>
+if (isset($_GET['email']) && isset($_GET['code'])) {
+    verifySubscription($_GET['email'], $_GET['code']);
+    echo "Subscription verified!";
+} else {
+    echo "Invalid request.";
+}
